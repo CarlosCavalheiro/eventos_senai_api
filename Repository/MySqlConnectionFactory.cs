141745d6da0b08b2b6bef746f25a66cd943dab20
Carlos Alexandre Cavalheiro <58004969+CarlosCavalheiro@users.noreply.github.com>
2024-05-24 01:14:45 -0300

    Update MySqlConnectionFactory.cs

--- Repository/MySqlConnectionFactory.cs
+++ Repository/MySqlConnectionFactory.cs
@@ -7,8 +7,8 @@ public class MySqlConnectionFactory
         const string connectionString = "server=189.50.105.10;" +
                                         "port=3306;" +
                                         "database=db_evento;" +
-                                        "uid=root;" +
-                                        "pwd=123456;";
+                                        "uid=user_eventos;" +
+                                        "pwd=senai792+;";
         return new MySqlConnection(connectionString);
     }
 }
