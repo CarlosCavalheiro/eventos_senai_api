8cbd646de648791fc005cda8bae6fec7e543a287
Carlos Alexandre Cavalheiro <58004969+CarlosCavalheiro@users.noreply.github.com>
2024-05-24 00:15:12 -0300

    publish
    publish

--- obj/Release/net8.0/api.AssemblyInfo.cs
+++ obj/Release/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39083c806942e5fe726748ce12a41c88abfd7f23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69f7381897f0100d0fd6a456e7e855bcb1c791b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
